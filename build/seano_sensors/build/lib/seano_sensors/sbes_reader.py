918ca486eac1f68294097b6f31cedba8f508c85f
Raihan Ryandhika <raihan.ryandhika24@gmail.com>
2026-03-25 17:44:47 +0700

    Fix Ready Test

--- build/seano_sensors/build/lib/seano_sensors/sbes_reader.py
+++ build/seano_sensors/build/lib/seano_sensors/sbes_reader.py
@@ -1,6 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float64
+from std_msgs.msg import Float64MultiArray
 import math
 import random
 import time
@@ -13,12 +13,13 @@ class SBESSim(Node):
         self.declare_parameter('sample_rate', 1.0)
         self.sample_rate = float(self.get_parameter('sample_rate').value)
 
-        self.publisher_ = self.create_publisher(Float64, '/sbes/data', 10)
+        self.publisher_ = self.create_publisher(Float64MultiArray, '/sbes/data', 10)
         self.timer = self.create_timer(1.0 / self.sample_rate, self.publish_sbes)
 
         self.start_time = time.time()
         self.base_depth = 12.0
         self.depth_amp = 1.5
+        self.temp_base = 28.0
 
         self.get_logger().info(
             f"SBES Dummy Started | topic=/sbes/data | sample_rate={self.sample_rate} Hz"
@@ -30,11 +31,23 @@ class SBESSim(Node):
         depth = self.base_depth + self.depth_amp * math.sin(t * 0.1)
         depth += random.uniform(-0.05, 0.05)
 
-        msg = Float64()
-        msg.data = float(depth)
+        water_temp = self.temp_base + 0.3 * math.sin(t * 0.03)
+        water_temp += random.uniform(-0.02, 0.02)
+
+        quality_flag = 1.0
+
+        msg = Float64MultiArray()
+        msg.data = [
+            round(depth, 3),
+            round(water_temp, 3),
+            quality_flag
+        ]
 
         self.publisher_.publish(msg)
-        self.get_logger().info(f"Depth: {depth:.3f} m")
+
+        self.get_logger().info(
+            f"Depth: {depth:.3f} m | Temp: {water_temp:.3f} C | Quality: {quality_flag:.0f}"
+        )
 
 
 def main():
